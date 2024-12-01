88fd1ac51914256d06e87e5944a86c8afc5728e7
Sagarbharti01 <sagarrsingh8923@gmail.com>
2024-12-01 15:13:35 +0530

    reverse of stack.

---
+++ stack05.java
@@ -0,0 +1,34 @@
+import java.util.Stack;
+
+public class stack05 {
+    public static void PushAtBottom(Stack<Integer> s, int data){
+        while (s.isEmpty()) {
+             s.push(data);
+            return ;
+        }
+        
+        int top=s.pop();
+        PushAtBottom(s,data);
+        s.push(top);
+    }
+    static void reverseStack(Stack<Integer> s){
+        while (s.isEmpty()) {
+            return ;
+        }
+ 
+        int top=s.pop();
+        reverseStack(s);
+        PushAtBottom(s,top);
+    }
+    public static void main(String[] args) {
+        Stack<Integer> s= new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        reverseStack(s);
+        // PushAtBottom(s,4);
+        while (!s.isEmpty()) {
+            System.out.println(s.pop());
+        }
+    }
+}
